640969342af9783a85b9cce0a88becfd1e7d20b6
Biitopher <biitopher@gmail.com>
2024-03-28 05:18:38 +0300

    [Feature]user file

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Index file """
+""" Index file that defines routes for user authentication"""
 
 from api.v1.views import app_views
 from api.v1.controllers.products import Products
