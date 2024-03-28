0ca52d93b5a2374a0c5c0071faeb1f7bfb440862
Biitopher <biitopher@gmail.com>
2024-03-28 16:33:00 +0300

    [Feature]Product file

---
+++ api/v1/controllers/products.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+from api.v1.controllers import db_storage
+from models.farmers import Farmer
+from models.products import Product
+import re
+
+images_list = ['maize', 'oranges', 'pineapples', 'apples', 'capsicum',
+               'onions', 'raspberries', 'tomatoes', 'rice', 'watermelon',
+               'melons', 'wheat']
+# Create a regex pattern using the keywords
+pattern = re.compile('|'.join(images_list), re.IGNORECASE)
+
+
+class Products:
+    """Class to define product functions"""
+
+    def list_all_products(self):
+        """Lists all products"""
+        products = (
+            db_storage.get_session()
+            .query(Product, Farmer)
+            .join(Farmer, Product.farmer_id == Farmer.id)
+            .all()
+        )
+        all_products = []
+
+        for product, farmer in products:
+            # Find the first match in the product name
+            match = pattern.search(product.name)
+
+            if match:
+                image = match.group()
+                product.image = image
+            else:
+                product.image = 'Logo'
+
+            all_products.append({"id": product.id,
+                                 "name": product.name,
+                                 "category": product.category,
+                                 "price": product.price,
+                                 "location": product.location,
+                                 "quantity": product.quantity,
+                                 "farmer_id": farmer.id,
+                                 "farmer": farmer.name,
+                                 "product_image": product.image
+                                 })
+        return all_products
