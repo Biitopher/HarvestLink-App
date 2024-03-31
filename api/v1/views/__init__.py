606bf5868547be16f0353bd56a8d090a921a520c
biitopher <biitopher@gmail.com>
2024-03-31 16:12:50 -0700

    [Fix]init file

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Creates init file """
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, template_folder='views')
+from api.v1.views.index import *
+from api.v1.views.user import *
