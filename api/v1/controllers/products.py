785ec769dc599f52b86e87ae4cdbfc0bb6a92932
Biitopher <biitopher@gmail.com>
2024-04-02 17:35:51 +0300

    [Fix]Adding more products on the list

--- api/v1/controllers/products.py
+++ api/v1/controllers/products.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Manages and displays products alongside their images"""
 from api.v1.controllers import db_storage
 from models.farmers import Farmer
 from models.products import Product
@@ -7,7 +7,7 @@ import re
 
 images_list = ['maize', 'oranges', 'pineapples', 'apples', 'capsicum',
                'onions', 'raspberries', 'tomatoes', 'rice', 'watermelon',
-               'melons', 'wheat']
+               'melons', 'wheat', 'beef', 'cheese', 'fish', 'broiler chicken', 'kienyeji chicken', 'milk', 'eggs']
 # Create a regex pattern using the keywords
 pattern = re.compile('|'.join(images_list), re.IGNORECASE)
 
@@ -46,36 +46,3 @@ class Products:
                                  "product_image": product.image
                                  })
         return all_products
-
-    def list_farmer_products(self, farmer_id):
-        """List all products added by farmer"""
-        products = (
-            db_storage.get_session()
-            .query(Product, Farmer)
-            .join(Farmer, Product.farmer_id == Farmer.id)
-            .filter(Farmer.id == farmer_id)
-            .all()
-        )
-        farmer_products = []
-
-        for product, farmer in products:
-            """Find the first match in the product name"""
-            match = pattern.search(product.name)
-
-            if match:
-                image = match.group()
-                product.image = image
-            else:
-                product.image = 'Logo'
-
-            farmer_products.append({"id": product.id,
-                                    "name": product.name,
-                                    "category": product.category,
-                                    "price": product.price,
-                                    "location": product.location,
-                                    "quantity": product.quantity,
-                                    "farmer_id": farmer.id,
-                                    "farmer": farmer.name,
-                                    "product_image": product.image
-                                    })
-        return farmer_products
