f302a3edcdf4197559ec748954209608d92d3116
Biitopher <biitopher@gmail.com>
2024-04-02 15:00:12 +0300

    [Fix]updated consumer and farmer routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,5 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, template_folder='views')
 from api.v1.views.index import *
 from api.v1.views.user import *
+from api.v1.views.consumer import *
+from api.v1.views.farmer import *
